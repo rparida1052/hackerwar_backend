8e841f863f46ee42c3ede181f448223e058a75a3
Rahul Parida <rparida1052@gmail.com>
2023-09-12 21:27:19 +0530

    add_imageOption

--- controller/wasteProductsController.js
+++ controller/wasteProductsController.js
@@ -1,94 +1,144 @@
 const Industry = require("../models/industrySchema");
 const WasteProducts = require("../models/wasteProductsSchema");
+const cloudinary = require('cloudinary');
 
+function isFileSupported(type, supportedTypes) {
+  return supportedTypes.includes(type);
+}
+
+async function uploadFileToCloudinary(file, folder, quality) {
+  const options = { folder };
+  options.resource_type = "auto";
+  if (quality) {
+    options.quality = quality;
+  }
+  return await cloudinary.uploader.upload(file.tempFilePath, options);
+}
 exports.addWasteProducts = async (req, res) => {
   try {
     const { industry, wasteName, wasteQuantity, wasteType } = req.body;
+    const file = req.files.imageFile;
+    console.log(industry,wasteName,wasteQuantity);
+    console.log("file -->",file)
+
+    
+
+    if (!file) {
+      return res.status(400).json({
+        success: false,
+        message: "Please Enter Valid file",
+      });
+    }
     if (!industry) {
       return res.status(400).json({
         success: false,
         message: "Please Enter Valid industryName",
       });
-    } if(!wasteName){
-        return res.status(400).json({
-            success: false,
-            message: "Please Enter Valid wasteName",
-          });
     }
-     if(!wasteQuantity){
-        return res.status(400).json({
-            success: false,
-            message: "Please Enter Valid wasteQuantity",
-          });
+    if (!wasteName) {
+      return res.status(400).json({
+        success: false,
+        message: "Please Enter Valid wasteName",
+      });
     }
-     if(!wasteType){
-        return res.status(400).json({
-            success: false,
-            message: "Please Enter Valid wasteType",
-          });
+    if (!wasteQuantity) {
+      return res.status(400).json({
+        success: false,
+        message: "Please Enter Valid wasteQuantity",
+      });
     }
+    if (!wasteType) {
+      return res.status(400).json({
+        success: false,
+        message: "Please Enter Valid wasteType",
+      });
+    }
+
+    const supportedTypes = ["jpg", "png", "jpeg"];
+    const fileExtension = file.name.split(".")[1].toLowerCase();
+
+    if (!isFileSupported(fileExtension, supportedTypes)) {
+      return res.status(400).json({
+        success: false,
+        message: "File format not supported",
+      });
+    }
+
+    const response = await uploadFileToCloudinary(file, "hackerwar");
+
     const wasteproducts = new WasteProducts({
-        industry,wasteName,wasteType,wasteQuantity
-    })
+      industry,
+      wasteName,
+      wasteType,
+      wasteQuantity,
+      wasteImage: response.secure_url,
+    });
 
     const saveWasteProducts = await wasteproducts.save();
-    const updatedindustryDetails = await Industry.findByIdAndUpdate(industry,{$push:{wasteProducts:saveWasteProducts._id}},{new:true}).populate("wasteProducts").exec();
-    updatedindustryDetails.industryPassword = undefined
+    const updatedindustryDetails = await Industry.findByIdAndUpdate(
+      industry,
+      { $push: { wasteProducts: saveWasteProducts._id } },
+      { new: true }
+    )
+      .populate("wasteProducts")
+      .exec();
+      console.log(updatedindustryDetails)
+    updatedindustryDetails.industryPassword = undefined;
     res.status(200).json({
-        success:true,
-        message:"Waste details updated successfully",
-        industry_Waste_Details : updatedindustryDetails
-    })
-
+      success: true,
+      message: "Waste details updated successfully",
+      industry_Waste_Details: updatedindustryDetails,
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
-        success: false,
-        message: "Internal Error occured in server",
-        error: error,
-      });
+      success: false,
+      message: "Internal Error occured in server",
+      error: error,
+    });
   }
 };
 
+exports.deleteWasteProducts = async (req, res) => {
+  try {
+    const { industry, id } = req.body;
 
-exports.deleteWasteProducts = async(req,res) =>{
-    try{
-        const {industry,id} = req.body;
-        
-    if(!id){
-        return res.status(400).json({
-            success: false,
-            message: "Please Enter Valid waste details",
-          });
+    if (!id) {
+      return res.status(400).json({
+        success: false,
+        message: "Please Enter Valid waste details",
+      });
     }
     //finding the waste
-    const waste = await WasteProducts.findById({_id:id})
-    if(!waste){
-        return res.status(400).json({
-            success: false,
-            message: "No waste found",
-          });
+    const waste = await WasteProducts.findById({ _id: id });
+    if (!waste) {
+      return res.status(400).json({
+        success: false,
+        message: "No waste found",
+      });
     }
     //delete from waste table
-    const deletedWaste = await WasteProducts.findByIdAndDelete({Industry:industry,_id:id}).exec();
+    const deletedWaste = await WasteProducts.findByIdAndDelete({
+      Industry: industry,
+      _id: id,
+    }).exec();
 
     //update from industry table
 
-        const updateIndustry = await Industry.findByIdAndUpdate(industry,{
-            $pull:{
-                wasteProducts:id
-            },
-        })
+    const updateIndustry = await Industry.findByIdAndUpdate(industry, {
+      $pull: {
+        wasteProducts: id,
+      },
+    });
     res.status(200).json({
-        success:true,
-        message:"Waste details deleted successfully",
-    })
-    }catch(error){
-        console.log(error)
-        res.status(500).json({
-            success:false,
-            message:"Internal server error"
-        })
-    }
-
-}
+      success: true,
+      message: "Waste details deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
+
 const routes = require('./routes/routes')
 const dbConfig = require('./config/databaseConfig')
+const cloudinary = require('./config/cloudinaryConfig');
+const fileUpload = require('express-fileupload');
 require('dotenv').config();
 app.use(express.json());
 
 
+app.use(fileUpload({
+    useTempFiles:true,
+    tempFileDir:"/temp/"
+}))
 app.use("/api/v1",routes)
 
 app.get('/',(req,res)=>{
@@ -16,3 +23,4 @@ app.listen(port,()=>{
     console.log(`App is listening on port${port}`)
 })
 dbConfig();
+cloudinary.cloudinaryConnect();
